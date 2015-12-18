a5e83f0c1cfe1de146f1912a394d152b6165ae92
Tim Finnell <tim.finnell@gmail.com>
2015-12-17 18:03:21 -0800

    Update export

--- app/templates/ExtendedComponent.js
+++ app/templates/ExtendedComponent.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import withStyles from '../../decorators/withStyles';
-<% if (styles) { %>
-<%- "import styles from './"+classname+".css';" %>
+<% if (styles) { %><%- "import styles from './"+classname+".css';" %><% } %>
 
-<% } %>
 @withStyles(styles)
 class <%= classname %> extends Component {
+  static propTypes {};
   render() {
-    return (<div></div>)
+    return (
+      <div></div>
+    );
   }
 }
+
+export default <%= classname %>;
