3f27bec1f7cb5544bb6b0f67ad20b497b9df403d
Tim Finnell <tim.finnell@gmail.com>
2016-03-06 04:21:29 -0800

    Remove style annotation

--- component/templates/ExtendedComponent.js
+++ component/templates/ExtendedComponent.js
@@ -1,12 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-<% if (nostyles === false) { -%>
-<%- "import withStyles from '../../decorators/withStyles';" %>
-<%- "import styles from './"+classname+".css';" %>
-<% } -%>
 
-<% if (nostyles === false) { -%>
-<%= '@withStyles(styles)' %>
-<% } -%>
 class <%= classname %> extends Component {
   static propTypes = {};
   render() {
