2331e8a8426678fce06ab1aee53b6b9037482d9e
Tim Finnell <tim.finnell@gmail.com>
2016-04-27 17:13:52 -0700

    Adjust styled component to use isomorphic-style-loader

--- component/templates/ExtendedComponent.js
+++ component/templates/ExtendedComponent.js
@@ -1,4 +1,8 @@
 import React, { Component, PropTypes } from 'react';
+<% if (!nostyles) { -%>
+import s from './<%= classname %>.scss';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+<% } -%>
 
 class <%= classname %> extends Component {
   static propTypes = {};
@@ -9,4 +13,9 @@ class <%= classname %> extends Component {
   }
 }
 
+<% if (!nostyles) { -%>
+export default withStyles(s)(<%= classname %>);
+<% } else { -%>
 export default <%= classname %>;
+<% } -%>
+
